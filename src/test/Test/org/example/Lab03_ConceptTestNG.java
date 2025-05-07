2f4303719fffff8f8f7f590c8d9ad0a96588c673
Mrunal2-tester <mail2mrunal@gmail.com>
2025-05-07 20:49:58 +0530

    Push

--- src/test/Test/org/example/Lab03_ConceptTestNG.java
+++ src/test/Test/org/example/Lab03_ConceptTestNG.java
@@ -1,4 +1,45 @@
 package org.example;
 
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
 public class Lab03_ConceptTestNG {
+    String pincode="440002";
+    @Test
+            public void positive_TC1()
+    {
+        pincode="440002";
+        RestAssured
+                .given()
+                .baseUri("https://zippopotam.us")
+                .basePath("/IN/440001")
+                .when()
+                .get()
+                .then().log().all().statusCode(200);
+    }
+    @Test
+    public void positive_TC2()
+    {
+        pincode="@";
+        RestAssured
+                .given()
+                .baseUri("https://zippopotam.us")
+                .basePath("/IN/440001")
+                .when()
+                .get()
+                .then().log().all().statusCode(200);
+    }
+    @Test
+    public void positive_TC3()
+    {
+        pincode="";
+        RestAssured
+                .given()
+                .baseUri("https://zippopotam.us")
+                .basePath("/IN/440001")
+                .when()
+                .get()
+                .then().log().all().statusCode(200);
+    }
+
 }
